378b35ab458d48757fcf2094a3521e12bbe02b06
Matt Clagett <mattrclagett@gmail.com>
2023-05-04 19:52:12 -0300

    made a cool form

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,16 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import { useState } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [formValue, setFormValue] = useState('')
+
+  function submitForm() {
+    alert(formValue)
+  }
+
   return (
     <>
       <Head>
@@ -15,7 +21,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        Hello
+        {formValue}
+        <input value={formValue} onChange={(event)=>setFormValue(event.target.value)}/>
+        <button onClick={submitForm}>Submit</button>
       </main>
     </>
   )
